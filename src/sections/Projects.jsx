5348343ca67795b35feb758bec61a37aeebb26c9
Jai Parasher <jaiparasher30@gmail.com>
2024-10-20 22:25:05 +0530

    fixed project computer model

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -86,12 +86,12 @@ const Projects = () => {
             <directionalLight position={[10, 10, 5]} />
             <Center>
               <Suspense fallback={<CanvasLoader />}>
-                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+              <group scale={2.9} position={[-0.43, -4.5, 0]} rotation={[0, -0.1, 0]}>
                   <DemoComputer texture={currentProject.texture} />
                 </group>
               </Suspense>
             </Center>
-            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
           </Canvas>
         </div>
       </div>
