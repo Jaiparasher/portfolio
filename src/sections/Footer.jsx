8c5ae003e51addb3d501befbc4d916a2a64db964
Jai Parasher <jaiparasher30@gmail.com>
2024-10-20 23:44:21 +0530

    added link in footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,21 @@ const Footer = () => {
         </div>
   
         <div className="flex gap-3">
+          <a href="https://github.com/Jaiparasher">
           <div className="social-icon">
             <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
           </div>
+          </a>
+          {/* <a href="">
           <div className="social-icon">
             <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
           </div>
+          </a> */}
+          <a href="https://www.instagram.com/jai_parasher30/">
           <div className="social-icon">
             <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
           </div>
+          </a>
         </div>
   
         <p className="text-white-500">© 2024 Jai Parasher. All rights reserved.</p>
