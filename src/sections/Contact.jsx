6e213587b77ddbf26e77a2ba96fe302b3d442dc8
Jai Parasher <jaiparasher30@gmail.com>
2024-10-20 23:57:06 +0530

    fixed contact component

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -16,6 +16,7 @@ const Contact = () => {
     setForm({ ...form, [name]: value });
   };
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -26,9 +27,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Jai Parasher',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'jaiparasher30@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
